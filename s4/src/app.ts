9c39d24e04f7e701a0292a0e4deef82d59a9ffeb
John Cevallos <joancemac@gmail.com>
2024-04-29 23:32:28 -0500

    Add typing for request and response in app.ts

--- s4/src/app.ts
+++ s4/src/app.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 import express, { Response} from 'express'
 
+// ejemplo de tipado de request y response
+
 
 const prisma = new PrismaClient()
 const app = express()
