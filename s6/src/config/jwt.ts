56705c710b3e8dbb718beab9369b0c7aede4c184
John Cevallos <joancemac@gmail.com>
2024-05-13 22:32:19 -0500

    adsf

--- s6/src/config/jwt.ts
+++ s6/src/config/jwt.ts
@@ -11,19 +11,11 @@ export class JwtAdapter {
     })
   }
   static validateToken<T>(token: string): Promise< T | null> {
-    
     return new Promise( (resolve) => {
-
       jwt.verify( token, JWT_SEED, (err, decoded) => {
-
         if( err ) return resolve(null);
-
         resolve( decoded as T);
-
       });
-
-
-
     })
   }
 }
