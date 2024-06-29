a6c97e80a402782e52ccd79cd0edc1f7afe7f659
John Cevallos <joancemac@gmail.com>
2024-06-29 16:53:43 -0500

    refactor: Update PruebaGateway to handle client connections and disconnections

--- a12/ejemplo2/src/prueba/prueba.gateway.ts
+++ a12/ejemplo2/src/prueba/prueba.gateway.ts
@@ -1,7 +1,50 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { PruebaService } from './prueba.service';
+import { Server, Socket } from 'socket.io';
+import { messageDto } from './dto/mensaje.dto';
 
-@WebSocketGateway()
-export class PruebaGateway {
+@WebSocketGateway({cors:true})
+export class PruebaGateway implements OnGatewayConnection, OnGatewayDisconnect{
+
+  @WebSocketServer()
+  wss:Server;
+
   constructor(private readonly pruebaService: PruebaService) {}
+
+  async handleConnection(client: Socket, ...args: any[]) {
+    const token = client.handshake.headers.authentication as string;
+    try {
+      // validar el token
+      await this.pruebaService.registerClient(client, token);
+    } catch (error) {
+      client.disconnect();  
+      return;    
+    }
+    this.wss.emit('clients-updated', this.pruebaService.getConnectedClients());
+  }
+  handleDisconnect(client: Socket) {
+    this.pruebaService.removeClient(client.id);
+    this.wss.emit('clients-updated', this.pruebaService.getConnectedClients());
+  }
+
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: messageDto) {
+    //! todos reciben el mensaje
+    this.wss.emit('message-from-server',{
+      fullName: this.pruebaService.getUserFullName(client.id),
+      message: payload.message || 'No message',
+    });
+
+    // el cliente que emite este evento NO recibe la respuesta
+    // client.broadcast.emit('message-from-server',{
+    //   fullName: this.pruebaService.getUserFullName(client.id),
+    //   message: payload.message || 'No message',
+    // })
+
+    // solo el cliente que emite este evento recibe la respuesta
+    // client.emit('message-from-server',{
+    //   fullName: this.pruebaService.getUserFullName(client.id),
+    //   message: payload.message || 'No message',
+    // })
+  }
 }
